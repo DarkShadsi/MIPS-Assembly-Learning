8665bdd9bb168ac733a214585d4d6697cc4288ef
DarkShadsi <depaudhonvincent24@gmail.com>
2026-07-16 19:50:01 +0800

    learned how to save a register to the stack

---
+++ 01-basics/saveRegisterToStack.asm
@@ -0,0 +1,47 @@
+.data
+	oldValueMessage: .asciiz "\nOld value: "
+	incrementedValueMessage: .asciiz "Incremented Value: "
+.text
+	# Note: By conventions, the callee should not modify the value of an $s register
+	main: 
+		addi $s0, $zero, 10
+		
+		jal incrementRegister
+		
+		# print the old value
+		li $v0, 4
+		la $a0, oldValueMessage
+		syscall
+		li $v0, 1
+		move $a0, $s0
+		syscall
+		
+		
+		
+	
+	# Tell the system that the program is done
+	li $v0, 10
+	syscall
+	
+	# A function to increase the value of a register
+	incrementRegister:
+		addi $sp, $sp, -4 # 'sp' stands for stack pointer
+		
+		# saves the value in  s0 to the first location in the stack pointer
+		sw $s0, 0($sp) # 0 is offset
+		
+		addi $s0, $s0, 30
+		
+		# print the new value of s0
+		li $v0, 4
+		la $a0, incrementedValueMessage
+		syscall
+		li $v0, 1
+		move $a0, $s0
+		syscall
+		
+		# load back the old value of s0
+		lw $s0, 0($sp)
+		addi $sp, $sp, 4
+		
+		jr $ra
