ff7c841641575ae968d39bf622f88327a36c7532
DarkShadsi <depaudhonvincent24@gmail.com>
2026-07-16 19:48:59 +0800

    Learned to remove a bit from a specific position

--- 01-basics/bitManipulation.asm
+++ 01-basics/bitManipulation.asm
@@ -1,22 +1,22 @@
 .data
 	prompt: .asciiz "Enter an integer: "
+	prompt2: .asciiz "\nEnter bit position you want removed: "
 	bitRepMessage: .asciiz "Binary representation: "
-	clearedBit: .asciiz "Cleared bit zero: "
+	clearedBit: .asciiz "Cleared n'th bit: "
 
 .text
-	# - This program demonstrates 'Clear Bit Zero', a bit manipulation where you remove the LSB (the rightmost)
-	# - For example: 1011 becomes 1010
-	# - This makes any bit an even one because odd bits became even and even bits stays even
+	# - This program demonstrates 'Clear Nth Bit', a bit manipulation where you remove a certain part of the binary
+	# - For example: 1011 becomes 1010 when we clear the first bit
 	main:
-		# get user input
+		# get user input (the number)
 		li $v0, 4
 		la $a0, prompt
 		syscall
 		li $v0, 5
 		syscall
-		
 		# store user input
-		move $t0, $v0
+		move $a2, $v0 # original number: should not be modified
+		addi $t0, $a2, 0 # copy the original number
 		
 		# print bit representation
 		li $v0, 4
@@ -24,8 +24,41 @@
 		syscall
 		jal printBitRep
 		
+		# get user input (position)
+		li $v0, 4
+		la $a0, prompt2
+		syscall
+		li $v0, 5
+		syscall
+		# store user input
+		addi $a1, $v0, -1
+		
+		# clear the n'th bit
+		addi $t0, $a2, 0 # copy the original number
+		jal removeBit
+		
+		# print the resulting bit
+		li $v0, 4
+		la $a0, clearedBit
+		syscall
+		jal printBitRep
+		
 		li $v0, 10
 		syscall
+		
+	removeBit:
+		# -  Idea: we make a mask with all 1's except the desired bit position
+		# - Then we 'and' it with the user input 
+		
+		# make the mask
+		li $t1, 1 # start with a single 1 in the righmost part: 0001
+		sllv $t1, $t1, $a1 # shift the bits n places to the left that is the place we want to mask out: 0100
+		nor $t2, $t1, $zero # 'nor' out to flip the bits: 1011. We now have the mask
+		
+		# 'and' the two numbers
+		and $t0, $t0, $t2
+		
+		jr $ra
 		
 	printBitRep:
 		addi $t1, $zero, 32 # counter: 32 iterations for 32 bits total
